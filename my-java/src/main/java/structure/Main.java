4686bf7a8f0cc9d7bc39c74ded2166c6db9d4e93
chinemeremichie@gmail.com <chinemeremichie@gmail.com>
2025-01-30 15:29:29 +0100

    exercise-1: Java practice

---
+++ my-java/src/main/java/structure/Main.java
@@ -0,0 +1,12 @@
+package structure;
+
+import bank.BankAccount;
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.accountNumber = 2345333;
+        account.accountHolder = "ichie chinemerem richard";
+        account.balance = 100000033.00;
+    }
+}
